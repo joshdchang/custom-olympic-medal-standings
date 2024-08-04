233feacf437968de70a351f1602e135a6d7a8bd5
Josh Chang <joshchang04@gmail.com>
2024-08-03 20:17:52 -0400

    add social stuff in head

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -15,6 +15,37 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="msapplication-TileColor" content="#ffc40d" />
+      <meta name="theme-color" content="#ffffff" />
+      <meta property="og:title" content="Build your own Paris 2024 medal standings" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://olympics.joshchang.co" />
+      <meta
+        property="og:image"
+        content="https://www.yourwebsite.com/og-image.png"
+      />
+      <meta
+        property="og:description"
+        content="Construct your own medal standings for the 2024 Summer Olympics in Paris by assigning different weights to gold, silver, and bronze medals."
+      />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
